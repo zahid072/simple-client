cf905e6e1637a8d2686cf5c2d7caa38de23b011a
zahid072 <jahidhasan19368@gmail.com>
2024-04-20 21:55:19 +0600

    post method added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Root = () => {
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const name = form.name.value;
+    const user = { name, email };
 
-    const handleSignIn = (e)=>{
-        e.preventDefault();
-        const form = e.target;
-        const email = form.email.value;
-        const name = form.name.value;
+    console.log(user)
 
-        
-
-    }
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        alert("success")
+    })
+  };
   return (
-    <>  
-       <div className="hero min-h-screen bg-base-200">
+    <>
+      <div className="hero min-h-screen bg-base-200">
         <div className="hero-content md:w-96">
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form onSubmit={handleSignIn} className="card-body">
-            <h1 className='text-3xl font-bold text-center'>Add user</h1>
+              <h1 className="text-3xl font-bold text-center">Add user</h1>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">name</span>
@@ -41,17 +53,16 @@ const Root = () => {
                   className="input input-bordered"
                   required
                 />
-               
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Sign In</button>
+                <button className="btn btn-primary">Add</button>
               </div>
             </form>
           </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
